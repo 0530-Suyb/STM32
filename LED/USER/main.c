4f1245405fe839ab582635e578efef73a0c36c19
0530-Suyb <2901967360@qq.com>
2021-01-29 08:46:08 +0800

    LED

---
+++ LED/USER/main.c
@@ -0,0 +1,19 @@
+#include "sys.h"
+#include "delay.h"
+#include "usart.h"
+#include "led.h"
+ 
+int main(void)
+{	
+  delay_init();
+	LED_Init();
+	while(1)
+	{
+		LED0=0;
+		LED1=1;
+		delay_ms(300);	 //—” ±300ms
+		LED0=1;
+		LED1=0;
+		delay_ms(300);	//—” ±300ms
+	}
+}
